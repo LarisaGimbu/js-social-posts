46c789d75d649dd4e2c673c4a4ffdb420bf02870
LarisaGimbu <gimbularisa@gmail.com>
2021-11-05 14:40:12 +0100

    first-commit

---
+++ js/script.js
@@ -0,0 +1,5 @@
+/*
+- Gli oggetti nell’array di oggetti rappresentano tutti i post. Ogni post dovrà avere le informazioni necessarie per stampare la relativa card: nome autore, foto profilo, data, testo del post, immagine (non tutti i post devono avere una immagine), numero di likes.
+- Stampiamo i post del nostro feed.
+- Sotto al nome NON inseire il calcolo della data (es 4 mesi fa) ma la data i formato italiano (GG/MM/AAAA)
+*/
