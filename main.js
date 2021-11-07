7dcd0bc69c5e0b1703ee7d540a7690e83d4ebb52
LarisaGimbu <gimbularisa@gmail.com>
2021-11-07 11:53:28 +0100

    main JS

--- main.js
+++ main.js
@@ -55,3 +55,66 @@ const posts = [
         "created": "2021-03-05"
     }
 ];
+
+const d = new Date();
+const day = d.getDay();
+const month = d.getMonth();
+const year = d.getFullYear();
+const date = `${day}/${month}/${year}`;
+
+const container = document.getElementById('container');
+container.innerHTML = '';
+
+//1. per ogni object creo un div con class post
+
+for(let index in posts){
+    const divPost = document.createElement('div');
+    divPost.classList = 'post';
+    container.append(divPost);
+
+    //2. dentro ogni div metto il contenuto del post
+    divPost.innerHTML =
+    `
+        <div class="post__header">
+            <div class="post-meta">                    
+                <div class="post-meta__icon">
+                    <img class="profile-pic" src="${posts[index].author.image} " alt="${posts[index].author.name}">                    
+                </div>
+                <div class="post-meta__data">
+                    <div class="post-meta__author">${posts[index].author.name} </div>
+                    <div class="post-meta__time">${date} </div>
+                </div>                    
+            </div>
+        </div>
+        <div class="post__text">${posts[index].content} </div>
+        <div class="post__image">
+            <img src="${posts[index].media}" alt="">
+        </div>
+        <div class="post__footer">
+            <div class="likes js-likes">
+                <div class="likes__cta">
+                    <a class="like-button  js-like-button" href="#" data-postid="1">
+                        <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                        <span class="like-button__label">Mi Piace</span>
+                    </a>
+                </div>
+                <div class="likes__counter"> 
+                    Piace a <b id="like-counter-1" class="js-likes-counter">${posts[index].likes}</b> persone
+                </div>
+            </div> 
+        </div> 
+    `
+}
+
+//Al click del like-button incrementare di uno i like
+
+let likeButton = document.getElementsByClassName('js-like-button');
+
+for(let i = 0; i < likeButton.length; i++){
+
+   likeButton[i].addEventListener('click', function(){
+    post[i].likesNumbers++
+
+    console.log(post[i].likesNumbers);
+  })
+  }
